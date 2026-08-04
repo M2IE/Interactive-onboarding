3207d02e536b0aea1d3f61872776e4d78c907cb1
isOdin-l <ivanbildagin@mail.ru>
2026-08-04 12:39:04 +0300

    chore: delete unused code

--- services/admin/cmd/admin/main.go
+++ services/admin/cmd/admin/main.go
@@ -1,5 +1,1 @@
 package admin
-
-func main() {
-
-}
--- services/widget/internal/delivery/http/handler.go
+++ services/widget/internal/delivery/http/handler.go
@@ -1,12 +1,1 @@
 package http
-
-type IService interface {
-}
-
-type Handler struct {
-	service IService
-}
-
-func NewHandler(s IService) *Handler {
-	return &Handler{service: s}
-}
