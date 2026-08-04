ca5bfd7b5e9398959be475a8a5a526dec1d4c214
isOdin-l <ivanbildagin@mail.ru>
2026-08-04 14:20:16 +0300

    chore: add openapi specs, generation go and ts by make

--- Makefile
+++ Makefile
@@ -4,3 +4,17 @@ up:
 
 migrate-up:
 	docker compose --profile migrate up migrate
+
+rest-gen-admin-go:
+	oapi-codegen -config api/openapi/v1/admin/dto.yaml api/openapi/v1/admin/specs.yaml
+	oapi-codegen -config api/openapi/v1/admin/server.yaml api/openapi/v1/admin/specs.yaml
+
+rest-gen-admin-ts:
+	npx openapi-typescript api/openapi/v1/admin/specs.yaml -o gen/rest/v1/ts/admin/admin.ts
+
+rest-gen-admin:
+	make rest-gen-admin-go
+	make rest-gen-admin-ts
+
+api-gen:
+	make rest-gen-admin
--- services/admin/internal/delivery/http/handler.go
+++ services/admin/internal/delivery/http/handler.go
@@ -1,1 +1,64 @@
 package http
+
+import (
+	"context"
+
+	apiv1 "github.com/M2IE/Interactive-onboarding/gen/rest/v1/go/admin"
+)
+
+type IService interface {
+}
+
+type Handler struct {
+	service IService
+}
+
+func NewHandler(s IService) apiv1.ServerInterface {
+	return apiv1.NewStrictHandler(Handler{
+		service: s,
+	}, nil)
+}
+
+// Scenario analytics
+// (GET /admin/analytics/{scenarioId})
+func (h Handler) GetAnalytics(ctx context.Context, request apiv1.GetAnalyticsRequestObject) (apiv1.GetAnalyticsResponseObject, error)
+
+// List scenarios
+// (GET /admin/scenarios)
+func (h Handler) ListScenarios(ctx context.Context, request apiv1.ListScenariosRequestObject) (apiv1.ListScenariosResponseObject, error)
+
+// Create scenario
+// (POST /admin/scenarios)
+func (h Handler) CreateScenario(ctx context.Context, request apiv1.CreateScenarioRequestObject) (apiv1.CreateScenarioResponseObject, error)
+
+// Get scenario
+// (GET /admin/scenarios/{id})
+func (h Handler) GetScenario(ctx context.Context, request apiv1.GetScenarioRequestObject) (apiv1.GetScenarioResponseObject, error)
+
+// Update scenario
+// (PATCH /admin/scenarios/{id})
+func (h Handler) UpdateScenario(ctx context.Context, request apiv1.UpdateScenarioRequestObject) (apiv1.UpdateScenarioResponseObject, error)
+
+// Publish scenario
+// (POST /admin/scenarios/{id}/publish)
+func (h Handler) PublishScenario(ctx context.Context, request apiv1.PublishScenarioRequestObject) (apiv1.PublishScenarioResponseObject, error)
+
+// Create step
+// (POST /admin/scenarios/{id}/steps)
+func (h Handler) CreateStep(ctx context.Context, request apiv1.CreateStepRequestObject) (apiv1.CreateStepResponseObject, error)
+
+// Reorder steps
+// (PUT /admin/scenarios/{id}/steps/order)
+func (h Handler) ReorderSteps(ctx context.Context, request apiv1.ReorderStepsRequestObject) (apiv1.ReorderStepsResponseObject, error)
+
+// Delete step
+// (DELETE /admin/scenarios/{id}/steps/{stepId})
+func (h Handler) DeleteStep(ctx context.Context, request apiv1.DeleteStepRequestObject) (apiv1.DeleteStepResponseObject, error)
+
+// Update step
+// (PATCH /admin/scenarios/{id}/steps/{stepId})
+func (h Handler) UpdateStep(ctx context.Context, request apiv1.UpdateStepRequestObject) (apiv1.UpdateStepResponseObject, error)
+
+// Unpublish scenario
+// (POST /admin/scenarios/{id}/unpublish)
+func (h Handler) UnpublishScenario(ctx context.Context, request apiv1.UnpublishScenarioRequestObject) (apiv1.UnpublishScenarioResponseObject, error)
